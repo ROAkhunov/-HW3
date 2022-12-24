6d7f4af0f7813bfb6d8f32f580c3f1a8889610c8
Rus <roakhunov@gmail.com>
2022-12-24 19:54:28 +0500

    add task 23

---
+++ task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9. -только вот 4 не может быть кубом целого числа
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int finish=Convert.ToInt32(Console.ReadLine());
+for (int start = 1; start <= finish; start++)
+{
+  double result = Math.Pow(start,3) ;
+   System.Console.Write(result+" ");
+}
